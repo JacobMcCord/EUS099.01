f30c61e3c3d51aaf3145ac783d23113215e36393
JacobMcCord <jacobwmccord@gmail.com>
2015-02-11 16:42:23 -0600

    Added Images

--- scripts/map.js
+++ scripts/map.js
@@ -1,75 +1,101 @@
 // code adapted from https://www.mapbox.com/mapbox.js/example/v1.0.0/markers-with-image-slideshow/
 
-var map = L.mapbox.map('map', 'eus09901.km73d5o2') 
-            // Construct a bounding box for this map that the user cannot
-			// move out of
-			var southWest = L.latLng(52.4934, 13.45867),
-			northEast = L.latLng(52.545311, 13.328738),
-			bounds = L.latLngBounds(southWest, northEast);
+var map = L.mapbox.map('map','jacobwmccord.l31595mc')						//('map')   // ('map','jacobwmccord.l31595mc')
+// Construct a bounding box for this map that the user cannot
+// move out of
+
+var southWest = L.latLng(52.4934, 13.45867),
+northEast = L.latLng(52.545311, 13.328738),
+bounds = L.latLngBounds(southWest, northEast);
 
 
 
-// Add custom popup html to each marker
-map.markerLayer.on('layeradd', function(e) {
-    var marker = e.layer;
-    var feature = marker.feature;
-    var images = feature.properties.images
-    var slideshowContent = '';
 
-    for(var i = 0; i < images.length; i++) {
-        var img = images[i];
 
-        slideshowContent += '<div class="image' + (i === 0 ? ' active' : '') + '">' +
-        '<img src="' + img[0] + '" />' +
-        '<div class="caption">' + img[1] + '</div>' +
-        '</div>';
-    }
+// uses OSM tiles just to make sure I don't run out of Mapbox data
+/*L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);*/
 
-    // Create custom popup content
-    var popupContent =  '<div id="' + feature.properties.id + '" class="popup">' +
-    '<h2>' + feature.properties.title + '</h2>' +
-    '<div class="slideshow">' +
-    slideshowContent +
-    '</div>' +
-    '<div class="cycle">' +
-    '<a href="#" class="prev">&laquo; Previous</a>' +
-    '<a href="#" class="next">Next &raquo;</a>' +
-    '</div>'
-    '</div>';
 
-    // http://leafletjs.com/reference.html#popup
-    marker.bindPopup(popupContent,{
-        closeButton: false,
-        maxWidth: 200,
+
+// creates popups for map features
+map.featureLayer.on('layeradd', function(e) {
+	var marker = e.layer;
+	var feature = marker.feature;
+	var images = feature.properties.images
+	var slideshowContent = '';
+
+	for(var i = 0; i < images.length; i++) {
+		var img = images[i];
+
+		slideshowContent += '<div class="image' + (i === 0 ? ' active' : '') + '">' +
+		img[0] +
+		'<div class="caption">' + img[1] + '</div>' +
+		'</div>';
+	}
+
+	// Create custom popup content
+	var popupContent =  '<div id="' + feature.properties.id + '" class="popup">' +
+	'<h2>' + feature.properties.title + '</h2>' +
+	'<div class="slideshow">' +
+	slideshowContent +
+	'</div>' +
+	'<div class="cycle">' +
+	'<a href="#" class="prev">&laquo; Previous</a>' +
+	'<a href="#" class="next">Next &raquo;</a>' +
+	'</div>'
+	'</div>';
+
+	// http://leafletjs.com/reference.html#popup
+	marker.bindPopup(popupContent,{
+		closeButton: false,
+		maxWidth: 200,
 		autoPan: true,
-        keepInView: true
-    });
+		keepInView: true
+
+	});
+
 });
 
+
+// Adds images to the map using Leaflet's imageOverlay function
+var imageURL = 'images/stadium.png',
+imageBounds = [[52.510232, 13.231320], [52.519018, 13.253973]];
+
+var overlay = L.imageOverlay(imageURL, imageBounds)
+overlay.addTo(map);
+
+// This part is SUPPOSED to put the image behind the polygon, but instead it just
+// insists on crushing my dreams . . .
+overlay.bringToBack;
+
 // Add features to the map
-map.markerLayer.setGeoJSON(geoJson);
+var feature = map.featureLayer.setGeoJSON(geoJson);
+feature.bringToFront;
+
 
 // This example uses jQuery to make selecting items in the slideshow easier.
 // Download it from http://jquery.com
 $('#map').on('click', '.popup .cycle a', function() {
-    var $slideshow = $('.slideshow'),
-    $newSlide;
+	var $slideshow = $('.slideshow'),
+	$newSlide;
 
-    if ($(this).hasClass('prev')) {
-        $newSlide = $slideshow.find('.active').prev();
-        if ($newSlide.index() < 0) {
-            $newSlide = $('.image').last();
-        }
-    } else {
-        $newSlide = $slideshow.find('.active').next();
-        if ($newSlide.index() < 0) {
-            $newSlide = $('.image').first();
-        }
-    }
+	if ($(this).hasClass('prev')) {
+		$newSlide = $slideshow.find('.active').prev();
+		if ($newSlide.index() < 0) {
+			$newSlide = $('.image').last();
+		}
+	} else {
+		$newSlide = $slideshow.find('.active').next();
+		if ($newSlide.index() < 0) {
+			$newSlide = $('.image').first();
+		}
+	}
 
-    $slideshow.find('.active').removeClass('active').hide();
-    $newSlide.addClass('active').show();
-    return false;
+	$slideshow.find('.active').removeClass('active').hide();
+	$newSlide.addClass('active').show();
+	return false;
 });
 
-map.setView([52.519593, 13.405671], 12);
+map.setView([52.509187, 13.303643], 14);
